8c8bb41bf70e5970df349c326b2b8237c2a2d305
viking-try <nadda@live.in>
2020-02-13 21:33:23 +0000

    build stage

---
+++ jenkins/build/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#copy jar
+cp -f java-app/target/*.jar jenkins/build/app.jar
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "*********Building Jar Image **********"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo " "
+cd jenkins/build/ && docker-compose build --no-cache
---
+++ jenkins/build/mvn.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "***********$(date)*****************"
+echo "***************Building JAR********************"
+ docker run --rm  -v $PWD/java-app:/app -v /root/.m2:/root/.m2  -w /app maven:3-alpine "$@"
+
