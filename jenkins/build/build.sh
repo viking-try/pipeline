b799b93d3b455fd874d816b988fc0fc5ae953396
viking-try <nadda@live.in>
2020-02-14 05:48:18 +0000

    work space

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #copy jar
+
 cp -f java-app/target/*.jar jenkins/build/app.jar
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo "*********Building Jar Image **********"
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo " "
-cd $WORKSPACE/jenkins/build/ && docker-compose build --no-cache
+cd /jenkins/build/ && docker-compose build --no-cache
--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
+WORKSP=/root/docker-stof/jenkins_home/workspace/pipeline-mvn-doc
 echo "***********$(date)*****************"
 echo "***************Building JAR********************"
- docker run --rm  -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2  -w /app maven:3-alpine "$@"
+docker run --rm  -v $WORKSP/java-app:/app -v /root/.m2:/root/.m2  -w /app maven:3-alpine "$@"
 
