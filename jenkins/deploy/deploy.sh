524cbfec0cb76ef8231329225b210b5e284853d8
viking-try <nadda@live.in>
2020-02-23 14:13:38 +0000

    StrictHostKeyChecking

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -2,6 +2,6 @@ echo $BUILD_NUMBER > /tmp/.authfile
 echo jar-exe >> /tmp/.authfile
 echo $PASSD >> /tmp/.authfile
 chmod 600 $WORKSPACE/file.pem 
-scp -i file.pem /tmp/.authfile ec2-user@35.170.242.182:/tmp/.authfile
-scp -i file.pem $WORKSPACE/jenkins/deploy/varset.sh $WORKSPACE/jenkins/deploy/docker-compose.yml  ec2-user@35.170.242.182:
+scp -i file.pem -o StrictHostKeyChecking=no /tmp/.authfile ec2-user@35.170.242.182:/tmp/.authfile
+scp -i file.pem -o StrictHostKeyChecking=no $WORKSPACE/jenkins/deploy/varset.sh $WORKSPACE/jenkins/deploy/docker-compose.yml  ec2-user@35.170.242.182:
 ssh  -i file.pem ec2-user@35.170.242.182 " sh ~/varset.sh "
