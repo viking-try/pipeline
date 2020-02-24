7654ea1b9ac58218b91dcd4291456a4310c9c3ff
viking-try <nadda@live.in>
2020-02-24 13:55:58 +0000

    error in deploy var

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -2,6 +2,7 @@ echo $BUILD_NUMBER > /tmp/.authfile
 echo jar-exe >> /tmp/.authfile
 echo $PASSD >> /tmp/.authfile
 chmod 600 $WORKSPACE/file.pem 
-scp -i $SSH_KEY  /tmp/.authfile ec2-user@$RMIP:/tmp/
-scp -i $SSH_KEY -o StrictHostKeyChecking=no $WORKSPACE/jenkins/deploy/varset.sh $WORKSPACE/jenkins/deploy/docker-compose.yml  ec2-user@$RMIP:
-ssh  -i $SSH_KEY ec2-user@$RMIP " sh ~/varset.sh "
+scp -i $SSH_KEY  /tmp/.authfile ec2-user@172.31.90.210:/tmp/
+scp -i $SSH_KEY  $WORKSPACE/jenkins/deploy/varset.sh $WORKSPACE/jenkins/deploy/docker-compose.yml  ec2-user@$172.31.90.210:
+ssh  -i $SSH_KEY ec2-user@$RMIP "hostname; ll"
+#ssh  -i $SSH_KEY ec2-user@$RMIP " sh ~/varset.sh "
