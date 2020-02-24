62d51fb962caa978811637da2c90e8fa8bfc9202
viking-try <nadda@live.in>
2020-02-24 13:59:06 +0000

    error in deploy var test2

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -3,6 +3,6 @@ echo jar-exe >> /tmp/.authfile
 echo $PASSD >> /tmp/.authfile
 chmod 600 $WORKSPACE/file.pem 
 scp -i $SSH_KEY  /tmp/.authfile ec2-user@172.31.90.210:/tmp/
-scp -i $SSH_KEY  $WORKSPACE/jenkins/deploy/varset.sh $WORKSPACE/jenkins/deploy/docker-compose.yml  ec2-user@$172.31.90.210:
+scp -i $SSH_KEY  $WORKSPACE/jenkins/deploy/varset.sh $WORKSPACE/jenkins/deploy/docker-compose.yml  ec2-user@172.31.90.210:
 ssh  -i $SSH_KEY ec2-user@$RMIP "hostname; ll"
 #ssh  -i $SSH_KEY ec2-user@$RMIP " sh ~/varset.sh "
