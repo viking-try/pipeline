96113bf29dcdccaf3b22d17cd7d874d038a1f149
viking-try <nadda@live.in>
2020-02-24 13:32:44 +0000

    mod

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -2,6 +2,6 @@ echo $BUILD_NUMBER > /tmp/.authfile
 echo jar-exe >> /tmp/.authfile
 echo $PASSD >> /tmp/.authfile
 chmod 600 $WORKSPACE/file.pem 
-scp -i $SSH_KEY -o StrictHostKeyChecking=no /tmp/.authfile ec2-user@$RMIP:/tmp/.authfile
+scp -i $SSH_KEY  /tmp/.authfile ec2-user@$RMIP:/tmp/
 scp -i $SSH_KEY -o StrictHostKeyChecking=no $WORKSPACE/jenkins/deploy/varset.sh $WORKSPACE/jenkins/deploy/docker-compose.yml  ec2-user@$RMIP:
 ssh  -i $SSH_KEY ec2-user@$RMIP " sh ~/varset.sh "
